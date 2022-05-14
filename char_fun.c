e7aab2d28b82e863184e3020546cfe87d5201d14
jhonkelem <99354658+jhonkelem@users.noreply.github.com>
2022-05-14 23:36:40 +0200

    Update char_fun.c

--- char_fun.c
+++ char_fun.c
@@ -1,99 +1,97 @@
 #include "shell.h"
 
 /**
- *  * _putchar - writes the character c to stdout
- *   * @c: The character to print
- *    *
- *     * Return: On success 1.
- *      * On error, -1 is returned, and errno is set appropriately.
- *       */
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
 int _putchar(char c)
 {
-		return (write(1, &c, 1));
+	return (write(1, &c, 1));
 }
 /**
- *  * _strncpy - copie a string
- *   * @dest:char
- *    *  @src:char
- *     * @n:int
- *      * Return:char
- *       */
+ * _strncpy - copie a string
+ * @dest:char
+ *  @src:char
+ * @n:int
+ * Return:char
+ */
 
 char *_strncpy(char *dest, char *src, int n)
 {
-	int i;
+int i;
 
-	i = 0;
-		while (i < n && *(src + i))
-				{
-						*(dest + i) = *(src + i);
-							i++;
-								}
-			while (i < n)
-					{
-							*(dest + i) = '\0';
-								i++;
-									}
-				return (dest);
+i = 0;
+	while (i < n && *(src + i))
+	{
+	*(dest + i) = *(src + i);
+	i++;
+	}
+	while (i < n)
+	{
+	*(dest + i) = '\0';
+	i++;
+	}
+	return (dest);
 }
 
 /**
- *  * _strlen - lenght of string
- *   * @s:char
- *    * Return:int
- *     */
+ * _strlen - lenght of string
+ * @s:char
+ * Return:int
+ */
 
 int _strlen(char *s)
 {
-		int i;
+	int i;
 
-				for (i = 0; s[i] != '\0'; i++)
-							{
-											continue;
-													}
-				return (i);
+		for (i = 0; s[i] != '\0'; i++)
+		{
+			continue;
+		}
+return (i);
 }
 
 /**
- *  * _atoi - convert to a int
- *   * @s:string
- *    * Return:int
- *     */
+ * _atoi - convert to a int
+ * @s:string
+ * Return:int
+ */
 int _atoi(char *s)
 {
-	int i, j, n, x;
+int i, j, n, x;
 
-		i = n = 0;
-			x = 1;
-				while ((s[i] < '0' || s[i] > '9') && (s[i] != '\0'))
-						{
-									if (s[i] == '-')
-													x *= -1;
-											i++;
-												}
-					j = i;
-						while ((s[j] >= '0') && (s[j] <= '9'))
-								{
-											n = (n * 10) + x * ((s[j]) - '0');
-													j++;
-														}
-							return (n);
+	i = n = 0;
+	x = 1;
+	while ((s[i] < '0' || s[i] > '9') && (s[i] != '\0'))
+	{
+		if (s[i] == '-')
+			x *= -1;
+		i++;
+	}
+	j = i;
+	while ((s[j] >= '0') && (s[j] <= '9'))
+	{
+		n = (n * 10) + x * ((s[j]) - '0');
+		j++;
+	}
+	return (n);
 }
 /**
- *  * _puts - print a string
- *   * @str:pointer char
- *    * return:void
- *     */
+ * _puts - print a string
+ * @str:pointer char
+ * return:void
+ */
 void _puts(char *str)
 {
-		int i;
+	int i;
 
-			for (i = 0; str[i] != '\0'; i++)
-					{
-								_putchar(str[i]);
-									}
-			_putchar('\n');
-			return;
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+_putchar('\n');
+return;
 }
-
-
